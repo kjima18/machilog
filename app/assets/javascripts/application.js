b1a63d1fc12fd8328b305f515b26f8cac97a2c13
EC2 Default User <ec2-user@ip-172-31-44-155.ap-northeast-1.compute.internal>
2019-08-31 06:09:18 +0000

    fill in layout by bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,9 +9,9 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//
+//= require jquery
+//= require bootstrap-sprockets
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
-//= require bootstrap-sprockets
 //= require_tree .
