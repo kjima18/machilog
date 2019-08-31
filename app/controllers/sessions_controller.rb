bff082f79c3329e4228d35eb1f46f912a756b3c8
EC2 Default User <ec2-user@ip-172-31-44-155.ap-northeast-1.compute.internal>
2019-08-31 09:05:35 +0000

    add login&logout function

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,41 @@
+class SessionsController < ApplicationController
+  def new
+  end
+  
+  def create
+    user = User.find_by(email_params)
+    if user && user.authenticate(password_params)
+      log_in user
+      redirect_to '/', success: 'ログインに成功しました'
+    else
+      flash.now[:danger] = 'ログインに失敗しました'
+      render :new
+    end
+  end
+  
+  def destroy
+    log_out
+    redirect_to '/', info: 'ログアウトしました'
+  end
+  
+  
+  private
+  def email_params
+    params.require(:session).permit(:email)
+  end
+  
+  def password_params
+    params.require(:session).permit(:password)
+  end
+  
+
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+  
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+
   get '/' => 'pages#index'
   get '/signup' => 'users#new'
 
   resources :users
+  
+  get    '/login' => 'sessions#new'
+  post   '/login' => 'sessions#create'
+  delete '/logout'=> 'sessions#destroy'
 end
