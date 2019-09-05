b0066520bb584e72e9df80b317bf88d041d5b156
EC2 Default User <ec2-user@ip-172-31-44-155.ap-northeast-1.compute.internal>
2019-09-05 16:37:17 +0000

    add mypage

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 15 } 
   
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
   
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}+\z/i
   validates :password, format: { with: VALID_PASSWORD_REGEX }
