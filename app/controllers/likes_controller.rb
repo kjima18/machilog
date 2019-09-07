c925af0ed62fd3a8f3a6693b2a2389ef7e54931f
EC2 Default User <ec2-user@ip-172-31-44-155.ap-northeast-1.compute.internal>
2019-09-07 09:34:20 +0000

    add like_function by Ajax

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,23 +1,14 @@
 class LikesController < ApplicationController
   def create
-    @like = Like.new
-    @like.user_id = current_user.id
-    @like.post_id = params[:post_id]
-    if @like.save
-      redirect_to posts_path, success: 'いいねしました'
-    else
-      redirect_to posts_path, danger: 'いいねに失敗しました'
-    end
+    @post = Post.find(params[:post_id])
+    like = current_user.likes.build(post_id: params[:post_id])
+    like.save
   end
   
   def destroy
-    @like = Like.find_by(user_id: current_user.id, post_id: params[:post_id])
-    @like.destroy if @like.present?
-    if @like.destroyed?
-      redirect_to posts_path, success: 'いいねを取り消しました'
-    else
-      redirect_to posts_path, success: 'いいねを取り消しできませんでした'
-    end
+    @post = Post.find(params[:post_id])
+    like = Like.find_by(user_id: current_user.id, post_id: params[:post_id])
+    like.destroy if like.present?
   end
     
 end
