28bdd7084743e28ac00087cb483180fb719fdaf2
EC2 Default User <ec2-user@ip-172-31-44-155.ap-northeast-1.compute.internal>
2019-09-11 00:03:53 +0000

    ページネーションを修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
-    @posts = Post.page(params[:page]).per(10).order(created_at: :desc)
+    @posts = Post.page(params[:page]).per(5).order(created_at: :desc)
   end
   
   def new
