7d2055fa7febd84cc7f5e615b6fd9911eb28b6a1
kojima <kjima18@gmail.com>
2019-09-16 07:04:29 +0000

    Fix-seeds.rbをdb/KEN_ALL.CSVから読み込むように修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,28 +1,12 @@
 require 'csv'
-require 'zip'
 
-DLURL           = "https://www.post.japanpost.jp/zipcode/dl/oogaki/zip/ken_all.zip"
-SAVEDIR         = "db/"
 CSVROW_PREFNAME = 6
 CSVROW_CITYNAME = 7
 
-savePath = ""
-
-open(URI.escape(DLURL)) do |file|
-  ::Zip::File.open_buffer(file.read) do |zf|
-    zf.each do |entry|
-      savePath = SAVEDIR + entry.name
-      zf.extract(entry, savePath) { true }
-    end
-  end
-end
-
-
-CSV.foreach(savePath, encoding: "Shift_JIS:UTF-8") do |row|
+CSV.foreach('db/KEN_ALL.CSV', encoding: "Shift_JIS:UTF-8") do |row|
   prefName = row[CSVROW_PREFNAME]
   cityName = row[CSVROW_CITYNAME]
   pref = Prefecture.find_or_create_by(:name => prefName)
   City.find_or_create_by(:name => cityName, prefecture_id: pref.id)
 end
 
-File.unlink savePath
