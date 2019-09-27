664eb17669f54d074530b6198f223677a6b36547
kojima <kjima18@gmail.com>
2019-09-27 05:06:51 +0000

    Fix-デフォルト画像表示を修正

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -25,7 +25,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
   #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  "rogo.png"
+    "rogo.png" 
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   end
 
